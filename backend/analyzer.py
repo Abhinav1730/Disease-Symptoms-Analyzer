9ab953230fc006143dbcaf8407cd28b6c727c702
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-07-22 23:44:49 +0530

    Initialising the backend along with the frontend

---
+++ backend/analyzer.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import uuid
+
+def analyzeSymptoms():
+    df=pd.read_csv('data/disease_dataset_info.csv')
+    userSymptoms=set([s.strip().lower() for s in userSymptoms])
+
+    scores={}
+    for _,row in df.iterrows():
+        disease=row["disease"]
+        diseaseSymptoms=set(row["symptoms"].split(","))
+        matchCount=len(userSymptoms & diseaseSymptoms)
+        total=len(diseaseSymptoms)
+        score=matchCount/total if total else 0
+        scores[disease] = round(score,2)
+
+    scores = dict(sorted(scores.items(),key=lambda x:x[1],reverse=True)[:5])
+
+    #creating plot
+    plt.figure(figsize=(8,5))
+    plt.barh(list(scores.keys()),list(scores.values()),color="skyblue")
+    plt.xlabel("Match Score")
+    plt.ylabel("Top Disease Matches")
+    filename = f'{uuid.uuid4().hex}.png'
+    path=os.path.join('static/plots',filename)
+    plt.savefig(path)
+    plt.close()
+
+    return scores,filename
---
+++ backend/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,jsonify,send_file
+from flask_cors import CORS
+from analyzer import analyzeSymptoms
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/analyze' , method=['POST'])
+def analyze():
+    data=request.get_json()
+    symptoms=data.get('symptoms',[])
+    if not symptoms:
+        return jsonify({"error":"No Symptoms Provided"}),400
+    
+    results,plotPath=analyzeSymptoms(symptoms)
+    return jsonify({"results":results,"plotUrl":f'/plot/{plotPath}'})
+    
+
+
+
+@app.route('/plot/<filename>')
+def getPlot(filename):
+    return send_file(f'static/plots/{filename}',mimetype='image/png')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
