c9a7f34fc72ac19cc15622d9fb7065ba83374980
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-07-23 01:41:57 +0530

    Updating app.py

--- backend/app.py
+++ backend/app.py
@@ -1,26 +1,25 @@
-from flask import Flask,request,jsonify,send_file
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from analyzer import analyzeSymptoms
 
-app=Flask(__name__)
+app = Flask(__name__)
 CORS(app)
 
-@app.route('/analyze' , method=['POST'])
+
+@app.route("/analyze", methods=["POST"])
 def analyze():
-    data=request.get_json()
-    symptoms=data.get('symptoms',[])
+    data = request.get_json()
+    symptoms = data.get("symptoms", [])
     if not symptoms:
-        return jsonify({"error":"No Symptoms Provided"}),400
-    
-    results,plotPath=analyzeSymptoms(symptoms)
-    return jsonify({"results":results,"plotUrl":f'/plot/{plotPath}'})
-    
+        return jsonify({"error": "No Symptoms Provided"}), 400
+
+    results, plotPath = analyzeSymptoms(symptoms)
+    return jsonify({"results": results, "plotUrl": f"/plot/{plotPath}"})
 
 
-
-@app.route('/plot/<filename>')
+@app.route("/plot/<filename>")
 def getPlot(filename):
-    return send_file(f'static/plots/{filename}',mimetype='image/png')
+    return send_file(f"static/plots/{filename}", mimetype="image/png")
 
 
 if __name__ == "__main__":
