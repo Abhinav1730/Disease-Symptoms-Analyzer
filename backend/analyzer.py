32ea41d92799cf8c9526e9a621cc2c21bcf832b1
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-07-23 01:56:12 +0530

    Updating analyzer.py

--- backend/analyzer.py
+++ backend/analyzer.py
@@ -4,29 +4,30 @@ import matplotlib.pyplot as plt
 import os
 import uuid
 
-def analyzeSymptoms():
-    df=pd.read_csv('data/disease_dataset_info.csv')
-    userSymptoms=set([s.strip().lower() for s in userSymptoms])
 
-    scores={}
-    for _,row in df.iterrows():
-        disease=row["disease"]
-        diseaseSymptoms=set(row["symptoms"].split(","))
-        matchCount=len(userSymptoms & diseaseSymptoms)
-        total=len(diseaseSymptoms)
-        score=matchCount/total if total else 0
-        scores[disease] = round(score,2)
+def analyzeSymptoms(userSymptoms):
+    df = pd.read_csv("data/disease_dataset_info.csv")
+    userSymptoms = set([s.strip().lower() for s in userSymptoms])
 
-    scores = dict(sorted(scores.items(),key=lambda x:x[1],reverse=True)[:5])
+    scores = {}
+    for _, row in df.iterrows():
+        disease = row["disease"]
+        diseaseSymptoms = set(row["symptoms"].split(","))
+        matchCount = len(userSymptoms & diseaseSymptoms)
+        total = len(diseaseSymptoms)
+        score = matchCount / total if total else 0
+        scores[disease] = round(score, 2)
 
-    #creating plot
-    plt.figure(figsize=(8,5))
-    plt.barh(list(scores.keys()),list(scores.values()),color="skyblue")
+    topScores = dict(sorted(scores.items(), key=lambda x: x[1], reverse=True)[:5])
+
+    # creating plot
+    plt.figure(figsize=(8, 5))
+    plt.barh(list(topScores.keys()), list(scores.values()), color="skyblue")
     plt.xlabel("Match Score")
     plt.ylabel("Top Disease Matches")
-    filename = f'{uuid.uuid4().hex}.png'
-    path=os.path.join('static/plots',filename)
+    filename = f"{uuid.uuid4().hex}.png"
+    path = os.path.join("static/plots", filename)
     plt.savefig(path)
     plt.close()
 
-    return scores,filename
+    return scores, filename
